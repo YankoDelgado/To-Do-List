27aee010ae37e1d2515c3a6a6eadba495bcdf4f2
YankoDelgado <129774332+YankoDelgado@users.noreply.github.com>
2025-03-20 02:19:08 -0300

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import { createPool } from "mysql2"
+
+const pool = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+})
+
+export default pool.promise()
+
