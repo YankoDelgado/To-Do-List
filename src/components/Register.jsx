62e00e00aa5202cfe3cc22345911c47a296717ba
YankoDelgado <129774332+YankoDelgado@users.noreply.github.com>
2025-03-29 14:15:12 -0300

    Update Register.jsx
    Se incorporan las funciones de back-end

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,22 +2,43 @@
 
 import { useState } from "react"
 import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap"
+import { useAuth } from "../context/AuthContext"
 
 const Register = ({onToggleForm}) => {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+    const [error, setError] = useState("")
+    const { register } = useAuth()
+    const [loading, setLoading] = useState(false)
 
-    //Alertas
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError("")
+    
+        if (password !== confirmPassword) {
+            return setError("Las contraseñas no coinciden")
+        }
+        
+        try {
+            setLoading(true)
+            await register(email, password, name)
+        } catch (error) {
+            setError(error.message)
+        } finally{
+            setLoading(false)
+        }
+    }
 
     return (
-        <Container className="mt-5">
-            <Row className="justify-content-center">
-                <Col md={6}>
+        <Container className="vh-100 d-flex align-items-center justify-content-center">
+            <Row className="w-100 justify-content-center">
+                <Col md={6} lg={4}>
                     <div className="bg-white p-4 rounded shadow-sm">
                         <h2 className="text-center mb-4">Registro</h2>
-                        <Form>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        <Form onSubmit={handleSubmit}>
                             <Form.Group className="mb-3">
                                 <Form.Label>Nombre</Form.Label>
                                 <Form.Control type="text" value={name} onChange={(e)=> setName(e.target.value)} required></Form.Control>
@@ -34,7 +55,9 @@ const Register = ({onToggleForm}) => {
                                 <Form.Label>Confirmar Contraseña</Form.Label>
                                 <Form.Control type="password" value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)} required></Form.Control>
                             </Form.Group>
-                            <Button variant="primary" type="submit" className="w-100 mb-3">Registrarse</Button>
+                            <Button variant="primary" type="submit" className="w-100 mb-3" disabled={loading}>
+                                {loading ? "Registrando..." : "Registrarse"}
+                            </Button>
                         </Form>
                         <div className="text-center">
                             <p>
