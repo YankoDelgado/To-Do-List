7c130182beb06cad347860f445e5ef8a876d7937
YankoDelgado <129774332+YankoDelgado@users.noreply.github.com>
2025-03-29 14:14:23 -0300

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,9 +3,9 @@
 import { createContext, useState, useEffect, useContext } from "react"
 import axios from "axios"
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => useContext(AuthContext);
 
 const API_URL = "http://localhost:5000/api" //Ajustar URL segun corresponda
 
@@ -35,21 +35,28 @@ export const AuthProvider = ({ children }) => {
     }
 
     const register = async (email, password, name) => {
+        console.log("Intentando registrar usuario:", email, name) // Debugging
         try {
+            console.log("URL de la API:", API_URL);
             const response = await axios.post(`${API_URL}/register`, {email, password, name})
+            console.log("Respuesta del servidor:", response.data) // Debugging
+
             const {token, user} = response.data
             localStorage.setItem("token",token)
             axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
             setCurrentUser(user)
             return user
         } catch (error) {
+            console.error("Error en el registro:", error.response?.data || error) // Debug
             throw new Error(error.response.data.message || "Error en el registro")
         } 
     }
 
     const login = async (email, password) => {
+        console.log("Intentando loguear usuario:", email) // Debugging
         try{
             const response = await axios.post(`${API_URL}/login`, {email, password})
+            console.log("Respuesta del servidor:", response.data) // Debugging
             const {token, user} = response.data
             localStorage.setItem("token",token)
             axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
@@ -66,11 +73,5 @@ export const AuthProvider = ({ children }) => {
         setCurrentUser(null)
     }
 
-    const value = {
-        currentUser,
-        register,
-        login,
-        logout,
-        loading,
-    }
+    return <AuthContext.Provider value={{ currentUser, register, login, logout, loading }}>{ children }</AuthContext.Provider>
 }
